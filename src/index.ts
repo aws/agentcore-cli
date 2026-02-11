78188b883d59a08488070b7b1f5289b7db93c7de
Tejas Kashinath <tkashina@amazon.com>
2026-02-11 13:42:56 -0500

    chore: update CDK dependency to agentcore-cdk
    - Update vended CDK imports from @aws/agentcore-l3-cdk-constructs to agentcore-cdk
    - Add agentcore-cdk@^0.1.0-alpha1.0 as dependency in vended package.json
    - Remove npm link workflow (ensureL3Link, postinstall script, LOCAL_L3_PATH)
    - Delete obsolete LOCAL_DEV_SETUP.md
    - Update documentation references

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * Main entry point for the agentcore package.
  * Exports public APIs from schema and lib modules.
  *
- * For CDK constructs, use @aws/agentcore-l3-cdk-constructs package.
+ * For CDK constructs, use agentcore-cdk package.
  */
 
 // Schema exports (types, constants, errors)
