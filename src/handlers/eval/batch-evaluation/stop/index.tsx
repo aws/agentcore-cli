d454e2a39ae25a00ed0014efba38c87c1043b788
jariy17 <tjariy+jariy17@users.noreply.github.com>
2026-09-17 21:54:45 +0000

    fix(eval): require ids for batch stop commands

--- src/handlers/eval/batch-evaluation/stop/index.tsx
+++ src/handlers/eval/batch-evaluation/stop/index.tsx
@@ -1,6 +1,5 @@
 import z from "zod";
 import { createHandler, flag } from "../../../../router";
-import { InputValidationError } from "../../../../errors";
 import { JsonRendererKey } from "../../../../tui";
 import type { Core } from "../../../types";
 import { coreOptsFromCtx } from "../../../utils";
@@ -9,13 +8,10 @@ export const createStopBatchEvaluationHandler = (core: Core) =>
   createHandler({
     name: "stop",
     description: "stop a running batch evaluation",
-    flags: [flag("id", "the ID of the batch evaluation to stop", z.string().optional())],
+    flags: [flag("id", "the ID of the batch evaluation to stop", z.string())],
     handle: async (ctx, flags) => {
-      const id = flags["id"];
-      if (!id) throw new InputValidationError("required option '--id <id>' not specified");
-
       ctx
         .require(JsonRendererKey)
-        .renderJson(await core.eval.stopBatchEvaluation(id, coreOptsFromCtx(ctx)));
+        .renderJson(await core.eval.stopBatchEvaluation(flags["id"], coreOptsFromCtx(ctx)));
     },
   });
--- src/handlers/eval/batch-insights/stop/index.tsx
+++ src/handlers/eval/batch-insights/stop/index.tsx
@@ -1,5 +1,4 @@
 import z from "zod";
-import { InputValidationError } from "../../../../errors";
 import { createHandler, flag } from "../../../../router";
 import { JsonRendererKey } from "../../../../tui";
 import type { Core } from "../../../types";
@@ -9,13 +8,10 @@ export const createStopBatchInsightsHandler = (core: Core) =>
   createHandler({
     name: "stop",
     description: "stop a running batch insights run",
-    flags: [flag("id", "the ID of the batch insights run to stop", z.string().optional())],
+    flags: [flag("id", "the ID of the batch insights run to stop", z.string())],
     handle: async (ctx, flags) => {
-      const id = flags["id"];
-      if (!id) throw new InputValidationError("required option '--id <id>' not specified");
-
       ctx
         .require(JsonRendererKey)
-        .renderJson(await core.eval.stopBatchEvaluation(id, coreOptsFromCtx(ctx)));
+        .renderJson(await core.eval.stopBatchEvaluation(flags["id"], coreOptsFromCtx(ctx)));
     },
   });
