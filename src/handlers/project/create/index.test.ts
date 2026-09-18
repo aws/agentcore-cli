82fe5c2ef337556a44ec77593ccbbe14868a41f8
Tejas Kashinath <tkashina@amazon.com>
2026-09-17 20:57:15 -0400

    test(project): drop a comment that restates the table

--- src/handlers/project/create/index.test.ts
+++ src/handlers/project/create/index.test.ts
@@ -1,8 +1,6 @@
 import { expect, test } from "bun:test";
 import { resolveScaffoldHarnessInput } from "./index";
 
-// The deployed HarnessName is `<project>_default_<harness>` capped at 40 characters, so a
-// project name of 15 characters is the longest that still fits doubled.
 test.each([
   ["at the 15-character threshold", "P".repeat(15), /^P{15}$/],
   ["one over the threshold", "P".repeat(16), /^P{9}_[0-9a-f]{5}$/],
