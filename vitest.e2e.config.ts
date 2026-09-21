427189ff332a539d5276048bc417e173afb1182d
Hweinstock <hkobew@amazon.com>
2026-09-21 18:26:44 +0000

    refactor(e2e): rename top level dir to e2eTest

--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config";
-import { TAGS } from "./test/constants.ts";
+import { TAGS } from "./e2eTest/constants.ts";
 
 export default defineConfig({
   test: {
-    include: ["test/**/**.test.ts"],
-    setupFiles: ["./test/setup.ts"],
+    include: ["e2eTest/**/**.test.ts"],
+    setupFiles: ["./e2eTest/setup.ts"],
     tags: Object.values(TAGS).map((name) => ({ name })),
     reporters: ["verbose"],
   },
